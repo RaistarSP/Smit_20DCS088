1bbc6b53b9cc798313b9b57d0180ac152bd195b6
Smit Patel <smitp4343@gmail.com>
2022-09-07 13:19:58 +0530

    Updates

---
+++ precache-manifest.29853877de8dd258315a6143a7f9b905.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c76e76b8bf2d997a55ddb63154f0ea2",
+    "url": "/Smit_20DCS088/index.html"
+  },
+  {
+    "revision": "5204efd19cd4bb25612c",
+    "url": "/Smit_20DCS088/static/css/2.a1840750.chunk.css"
+  },
+  {
+    "revision": "f981d88d5be8b59652d2",
+    "url": "/Smit_20DCS088/static/css/main.6777df22.chunk.css"
+  },
+  {
+    "revision": "5204efd19cd4bb25612c",
+    "url": "/Smit_20DCS088/static/js/2.29c5b276.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Smit_20DCS088/static/js/2.29c5b276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f981d88d5be8b59652d2",
+    "url": "/Smit_20DCS088/static/js/main.0319d4f7.chunk.js"
+  },
+  {
+    "revision": "f6144cd6e03659fdab43",
+    "url": "/Smit_20DCS088/static/js/runtime-main.045a803b.js"
+  }
+]);
